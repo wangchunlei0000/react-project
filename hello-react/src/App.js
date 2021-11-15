49168a24671253e157efd09286fb7d61e1b3d87b
wangcl <wangcl@lenovocloud.com>
2021-11-15 11:42:49 +0800

    目录引入简写

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Hello from './components/Hello/Hello'
+import Hello from './components/Hello'
 
 export default class App extends Component {
   render () {
