7f45ba2fb92e90a205a0224eff46fa4757f1cb27
wangcl <wangcl@lenovocloud.com>
2021-12-29 16:50:06 +0800

    更换react-router-dom版本

--- hello-react/07_src_路由的基本使用/App.jsx
+++ hello-react/07_src_路由的基本使用/App.jsx
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
 import { Link, Route } from 'react-router-dom'
-import Header from './components/Header' // 一般组件
-import Home from './pages/Home' // 路由组件
-import About from './pages/About' // 路由组件
+import Home from './components/Home'
+import About from './components/About'
 import './App.css'
 export default class App extends Component {
 
   render() {
     return (
       <div className="app-wrapper">
-        <Header />
+        <div className="app-header">React Router Demo</div>
         <div className="app-body">
           <div className="nav">
             {/* 在 React 中靠路由链接实现切换组件 -- 编写路由链接 */}
@@ -18,8 +17,8 @@ export default class App extends Component {
           </div>
           <div className="content">
             {/* 注册路由 */}
-              <Route path="/home" component={ Home }/>
-              <Route path="/about" component={ About }/>
+          <Route path="/home" component={ Home }/>
+          <Route path="/about" component={ About }/>
           </div>
         </div>
       </div>
