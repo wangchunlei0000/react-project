01f8e9b60c5190e07760a86c94a0f5d78abf3948
wangcl <wangcl@lenovocloud.com>
2021-12-29 16:13:07 +0800

    react-router-dom

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
-import axios from 'axios'
-import Search from './components/Search'
-import List from './components/List'
+import { Link, Route , Routes} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
 import './App.css'
 export default class App extends Component {
 
   render() {
     return (
       <div className="app-wrapper">
-        <Search />
-        <List />
+        <div className="app-header">React Router Demo</div>
+        <div className="app-body">
+          <div className="nav">
+            {/* 在 React 中靠路由链接实现切换组件 -- 编写路由链接 */}
+            <Link to="/home" className="nav-item">Home</Link>
+            <Link to="/about" className="nav-item">About</Link>
+          </div>
+          <div className="content">
+            {/* 注册路由 */}
+            <Routes>
+              <Route path="/home" element={ <Home/> }/>
+              <Route path="/about" element={ <About/> }/>
+            </Routes>
+          </div>
+        </div>
       </div>
     )
   }
