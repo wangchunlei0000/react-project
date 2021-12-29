b2a05de1d68ebfa877fff053e2408c3bb6a0cad6
wangcl <wangcl@lenovocloud.com>
2021-12-29 17:15:06 +0800

    navlink

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Link, Route } from 'react-router-dom'
+import { NavLink, Route } from 'react-router-dom'
 import Header from './components/Header' // 一般组件
 import Home from './pages/Home' // 路由组件
 import About from './pages/About' // 路由组件
@@ -13,8 +13,8 @@ export default class App extends Component {
         <div className="app-body">
           <div className="nav">
             {/* 在 React 中靠路由链接实现切换组件 -- 编写路由链接 */}
-            <Link to="/home" className="nav-item">Home</Link>
-            <Link to="/about" className="nav-item">About</Link>
+            <NavLink activeClassName="active-style" to="/home" className="nav-item">Home</NavLink>
+            <NavLink activeClassName="active-style" to="/about" className="nav-item">About</NavLink>
           </div>
           <div className="content">
             {/* 注册路由 */}
--- hello-react/src/pages/Home/index.jsx
+++ hello-react/src/pages/Home/index.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export default class Home extends Component {
   render() {
-    console.log('-----------', this.props)
+    // console.log('-----------', this.props)
     return (
       <div>Homehhhhhhhhhhhhhhhhhhhhhhhhhhh</div>
     )
