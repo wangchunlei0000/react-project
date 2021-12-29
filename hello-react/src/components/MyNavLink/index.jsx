67254736f72e1efe68a51755e175a6da0000fcab
wangcl <wangcl@lenovocloud.com>
2021-12-29 17:30:18 +0800

    接收全部props

--- hello-react/src/components/MyNavLink/index.jsx
+++ hello-react/src/components/MyNavLink/index.jsx
@@ -3,8 +3,9 @@ import { NavLink } from 'react-router-dom'
 
 export default class MyNavLink extends Component {
   render() {
+    const { name } = this.props
     return (
-      <NavLink activeClassName="active-style" to={ this.props.to } className="nav-item">{ this.props.name }</NavLink>
+      <NavLink activeClassName="active-style" {...this.props} className="nav-item">{ name }</NavLink>
     )
   }
 }
