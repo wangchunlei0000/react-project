2e2412fd2edfa6f91deabed5b13df115b92b47f9
wangcl <wangcl@lenovocloud.com>
2021-12-30 15:42:14 +0800

    路由组件接收params参数

---
+++ hello-react/src/pages/Home/Message/Detail/index.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+const detailData = [
+  { id: '001', content: 'hello wang' },
+  { id: '002', content: 'hello chun' },
+  { id: '003', content: 'hello lei' },
+]
+
+export default class Detail extends Component {
+  render() {
+    // console.log('this.props=======detail', this.props)
+    const { match: { params : {id, title} } } = this.props
+    const findResult = detailData.find(item => {
+      return item.id === id
+    })
+    return (
+      <ul>
+        <li>ID: {id} </li>
+        <li>TITLE: {title}</li>
+        <li>CONTENT: {findResult.content}</li>
+      </ul>
+    )
+  }
+}
